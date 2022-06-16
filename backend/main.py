ca010e9d4e127e136bdc76bda4b8cef0d13b7ab8
Mohamed Baalla <developer.baalla@gmail.com>
2022-06-16 07:18:06 +0100

    first commit

---
+++ backend/main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, HTTPException
+import models
+from db import engine
+from sqlalchemy.orm import Session, joinedload
+from fastapi.middleware.cors import CORSMiddleware
+
+#create the database tables on app startup or reload
+models.Base.metadata.create_all(bind=engine)
+
+#initailize FastApi instance
+app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+#sentence_keywords endpoint
+@app.get("/sentence_keywords")
+def sentence_keywords_list():
+    # create a new database session
+    session = Session(bind=engine, expire_on_commit=False)
+
+    try:
+        # get all sentence_keywords items
+        sentence_keywords = session.query(models.Sentence).options(joinedload(models.Sentence.sentence_keywords)).order_by(models.Sentence.date.desc()).all()
+        # with_entities(models.Sentence.id).filter(models.Sentence.id == 20)
+
+    except Exception as ex:
+        raise HTTPException(status_code=404, detail=str(ex))
+
+    finally:
+        # close the session
+        session.close()
+
+    return sentence_keywords
+
+
+#young_people endpoint
+@app.get("/young_people")
+def young_people_list():
+    # create a new database session
+    session = Session(bind=engine, expire_on_commit=False)
+
+    try:
+        # get all young_people items
+        young_people_list = session.query(models.YoungPeople).with_entities(models.YoungPeople.id, models.YoungPeople.date, models.YoungPeople.logits, models.YoungPeople.net_sent, models.YoungPeople.logits_mean).order_by(models.YoungPeople.date.asc()).all()
+
+    except Exception as ex:
+        raise HTTPException(status_code=404, detail=str(ex))
+
+    finally:
+        # close the session
+        session.close()
+
+    return young_people_list
---
+++ readme.txt
@@ -0,0 +1,58 @@
+# My App
+
+> It is an application that consumes the API to fetch data and display it as a graph:
+- Create the database using Postgresql.
+- Create the backend using FastAPI/Python.
+- Create the frontend using ReactJS/D3.JS.
+
+**Database schema diagram**
+![Database schema diagram](attachments/database.png)
+
+**First Knowledge Graph**
+> It is a graph showing the sentences and associated keywords in the form of a network:
+![knowledge graph](attachments/Graph1.PNG)
+**Second Tree Graph**
+> It is a graph showing the sentences and associated keywords in the form of a tree:
+![Tree graph](attachments/Graph2.PNG)
+**Third Bar Graph**
+> It is a graph showing the logits and some other information in the form of a bar:
+![Bar graph](attachments/Graph3.PNG)
+
+## Built With
+
+- FastAPI
+- React
+- D3.js
+
+## Getting Started
+
+To get a local copy up and running follow these simple example steps.
+
+### Prerequisites
+- VS Code
+- chrome
+
+## Setup
+git clone https://github.com/Logibaalla/FastAPI-ReactJS-D3.git
+cd FastAPI-ReactJS-D3
+code.
+
+## Authors
+
+👤 **Mohamed Baalla**
+
+- GitHub: [@developerbaalla](https://github.com/developerbaalla)
+- Twitter: [@MohamedBaalla15](https://twitter.com/MohamedBaalla15)
+- LinkedIn: [mohamed-baalla-18039619a](https://linkedin.com/in/mohamed-baalla-18039619a)
+
+
+
+## Acknowledgments
+
+- Hat tip to anyone whose code was used
+- Inspiration
+- etc
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
